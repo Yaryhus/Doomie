d56d6b97a2037333fd036e707b0101c9b54fc74f
Guillermo A <duckbeardgd@gmail.com>
2020-11-19 19:58:04 +0100

    Metidas cosas de cambios

--- SwordPrototype/Assets/Code/SlicerPlane.cs
+++ SwordPrototype/Assets/Code/SlicerPlane.cs
@@ -1,10 +1,13 @@
-﻿using EzySlice;
+﻿using EZCameraShake;
+using EzySlice;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class SlicerPlane : MonoBehaviour
 {
@@ -26,21 +29,44 @@ public class SlicerPlane : MonoBehaviour
     int slicesUsed = 0;
     [SerializeField]
     float timeAvailable = 5.0f;
-    float timeUsed = 0;
+    float timeRemaining = 0;
+    [SerializeField]
+    float timeToRecoverSlice = 1.0f;
+    float currentRecoverTime = 0f;
+
+    [SerializeField]
+    [Range(0, 1)]
+    float slowmotion = 0.5f;
 
     [Header("Sounds")]
     [SerializeField]
     [Tooltip("Slice Sound variables: 1 - Air Slash, 2: Flesh Impact, 3: Object Impact, 4: Non Slashable object")]
+    Sound SliceSound = null;
+    [SerializeField]
     Sound idleSound = null;
     [SerializeField]
-    Sound SliceSound = null;
-    /*
-     * Slice Sound variables: 
-     * 1: Air Slash
-     * 2: Flesh Impact
-     * 3: Object Impact
-     * 4: Non Slashable object
-     */
+    Sound rechargeSliceSound = null;
+
+    [Header("Shake")]
+    [SerializeField]
+    [Tooltip("Suavidad del tiemble. Valores bajos son mas suaves")]
+    float shakeRoughness = 1.0f;
+    [SerializeField]
+    [Tooltip("Intensidad del tiemble")]
+    float shakeMagnitude = 1.0f;
+    [SerializeField]
+    [Tooltip("Cuanto tarda en ocurrir")]
+    float shakeFadeInTime = 0.1f;
+    [SerializeField]
+    [Tooltip("Cuanto tarda en irse")]
+    float shakeFadeOutTime = 1f;
+
+    [Header("HUD")]
+    [SerializeField]
+    TextMeshProUGUI text;
+    Image slowMoBar;
+
+    bool isInBladeMode;
 
     // Start is called before the first frame update
     void Start()
@@ -49,6 +75,7 @@ public class SlicerPlane : MonoBehaviour
         cutPlane.gameObject.SetActive(false);
         idleSound.Init();
         SliceSound.Init();
+        rechargeSliceSound.Init();
     }
 
     // Update is called once per frame
@@ -57,9 +84,13 @@ public class SlicerPlane : MonoBehaviour
         //If holding Right click, we slow time and activate "Blade mode"
         if (Input.GetMouseButton(1))
         {
+            //Empezamos a contar tiempo en slowmo hasta que se le gaste.
+            isInBladeMode = true;
+
             //Debug.Log("Tiempo lentooo");
             //Slow time code
-            Time.timeScale = 0.7f;
+            Time.timeScale = slowmotion;
+            Time.fixedDeltaTime = Time.timeScale * (slowmotion / 10f);
             cutPlane.gameObject.SetActive(true);
             //Block vertical movement of camera
             cam.GetComponent<FPSLookController>().LockVerticalCamera(true);
@@ -76,16 +107,45 @@ public class SlicerPlane : MonoBehaviour
                 Slice();
             }
         }
-        //Disable "Blade Mode"
+        //Disable "Blade Mode" voluntarily
         else if (Input.GetMouseButtonUp(1))
         {
             //Debug.Log("Se acabó el tiempo lentooo");
             //Reset everything: Time, unlock vertical view, restore postprocess, reset the slice plane.
-            Time.timeScale = 1.0f;
+            //Time.timeScale = 1.0f;
             cam.GetComponent<FPSLookController>().LockVerticalCamera(false);
             ChangePostProcessVolume(standartPP);
             ResetSlicePlane();
             cutPlane.gameObject.SetActive(false);
+            isInBladeMode = false;
+        }
+        //update text
+        text.SetText(slicesAvailable - slicesUsed + " / " + slicesAvailable);
+
+        //Recover from bulletTime
+        Time.timeScale += (1f / 1.1f) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+
+
+        if (!isInBladeMode)
+        {
+            //Recargar espadazos
+            if (slicesUsed > 0)
+            {
+                currentRecoverTime += Time.deltaTime;
+                if (currentRecoverTime >= timeToRecoverSlice)
+                {
+                    slicesUsed--;
+                    currentRecoverTime = 0f;
+                    rechargeSliceSound.PlayOneShot(transform);
+                }
+            }
+            //recargar tiempo de Slowmo
+            if(timeRemaining < timeAvailable)
+            {
+                timeRemaining += Time.deltaTime;
+                if()
+            }
 
         }
     }
@@ -114,41 +174,50 @@ public class SlicerPlane : MonoBehaviour
 
     public void Slice()
     {
-        Collider[] hits = Physics.OverlapBox(cutPlane.position, new Vector3(10, 0.1f, 10), cutPlane.rotation, layerMask);
-
-        //Hits air or nonSlashable object
-        if (hits.Length <= 0)
-        {
-            //hits air sound
-            SliceSound.SetParameter("SliceMaterial", 0);
-            SliceSound.Play(transform);
+        //If we already sliced maximun ammount of times
+        if (slicesUsed >= slicesAvailable)
             return;
-        }
-
-        for (int i = 0; i < hits.Length; i++)
+        else
         {
-            SlicedHull hull = SliceObject(hits[i].gameObject, null);
+            //Increment slice and do the rest
+            slicesUsed++;
+            Collider[] hits = Physics.OverlapBox(cutPlane.position, new Vector3(10, 0.1f, 10), cutPlane.rotation, layerMask);
+            CameraShaker.Instance.ShakeOnce(shakeMagnitude, shakeRoughness, shakeFadeInTime, shakeFadeOutTime);
 
-            //Enemy damage
-            EnemyBehavior enemy = hits[i].transform.GetComponent<EnemyBehavior>();
-            if (enemy != null)
+            //Hits air or nonSlashable object
+            if (hits.Length <= 0)
             {
-                SliceSound.SetParameter("SliceMaterial", 1);
-                enemy.TakeDamage(500);
-
+                //hits air sound
+                SliceSound.SetParameter("SliceMaterial", 0);
+                SliceSound.PlayOneShot(transform);
+                return;
             }
-            else
-                SliceSound.SetParameter("SliceMaterial", 2);
 
-            SliceSound.Play(transform);
-
-            if (hull != null)
+            for (int i = 0; i < hits.Length; i++)
             {
-                GameObject bottom = hull.CreateLowerHull(hits[i].gameObject, null);
-                GameObject top = hull.CreateUpperHull(hits[i].gameObject, null);
-                AddHullComponents(bottom);
-                AddHullComponents(top);
-                Destroy(hits[i].gameObject);
+                SlicedHull hull = SliceObject(hits[i].gameObject, null);
+
+                //Enemy damage
+                EnemyBehavior enemy = hits[i].transform.GetComponent<EnemyBehavior>();
+                if (enemy != null)
+                {
+                    SliceSound.SetParameter("SliceMaterial", 1);
+                    enemy.TakeDamage(500);
+
+                }
+                else
+                    SliceSound.SetParameter("SliceMaterial", 2);
+
+                SliceSound.PlayOneShot(transform);
+
+                if (hull != null)
+                {
+                    GameObject bottom = hull.CreateLowerHull(hits[i].gameObject, null);
+                    GameObject top = hull.CreateUpperHull(hits[i].gameObject, null);
+                    AddHullComponents(bottom);
+                    AddHullComponents(top);
+                    Destroy(hits[i].gameObject);
+                }
             }
         }
 
@@ -164,7 +233,7 @@ public class SlicerPlane : MonoBehaviour
         collider.convex = true;
 
         rb.AddExplosionForce(150, go.transform.position, 10);
-        
+
         //Adding Self destruct script.
         go.AddComponent<FadeAndDestroy>();
 
